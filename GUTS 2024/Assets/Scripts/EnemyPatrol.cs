9272732a608a137080d28a80b1c92e3d942fb590
Melissa Dorrian <melissadorrian04@gmail.com>
2024-10-12 21:11:02 +0100

    enemy movement added

---
+++ GUTS 2024/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System.Numerics;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public int targetPoint;
+    public float speed;
+    public int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPoint=0;
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed* Time.deltaTime);
+        if (Vector3.Distance(patrolPoints[targetPoint].position, transform.position)<2){
+            increaseTargetInt();
+        }
+    }
+
+
+
+    void increaseTargetInt(){
+        targetPoint++;
+        if (targetPoint==patrolPoints.Length){
+            targetPoint = 0;
+        }
+    }
+}
