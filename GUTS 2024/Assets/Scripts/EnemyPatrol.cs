05c1911e84ed32e666a200e76306fb01bddca66e
Michael Baxendale <mike.a.baxendale@gmail.com>
2024-10-13 01:22:35 +0100

    Enemies made

--- GUTS 2024/Assets/Scripts/EnemyPatrol.cs
+++ GUTS 2024/Assets/Scripts/EnemyPatrol.cs
@@ -6,38 +6,47 @@ using UnityEngine;
 public class EnemyPatrol : MonoBehaviour
 {
     public Transform[] patrolPoints;
-    public int targetPoint;
+    public int targetPoint = 0;
     public float speed;
-    public int count;
+    public int count = 0;
+    private bool huntingPlayer = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        targetPoint=0;
-        
+        this.transform.position = patrolPoints[0].transform.position;
     }
 
     void FixedUpdate()
     {
-        if (Vector3.Distance(patrolPoints[targetPoint].position,GameObject.FindGameObjectWithTag("Player").transform.position)<10){
-            transform.position = Vector3.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, speed* Time.deltaTime);
-            transform.position = Vector3.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, speed* Time.deltaTime);
-           // transform.position = Vector3.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, speed* Time.deltaTime);
-
-           // }
+        if (Vector3.Distance(this.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < 10 && huntingPlayer){
+            Debug.Log("Player Found");
+            transform.position = Vector3.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, speed);
         }
-        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed* Time.deltaTime);
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed);
 
-        if (Vector3.Distance(patrolPoints[targetPoint].position, transform.position)<=0.2){
-            increaseTargetInt();
+            if (Vector3.Distance(patrolPoints[targetPoint].position, transform.position) <= 0.2)
+            {
+                increaseTargetInt();
+            }
         }
+        
     }
 
 
 
     void increaseTargetInt(){
+        huntingPlayer == true
         targetPoint++;
         if (targetPoint==patrolPoints.Length){
             targetPoint = 0;
         }
+    }
+
+    public void setHuntingPlayer(bool newValue)
+    {
+        huntingPlayer = newValue;
     }
 }
